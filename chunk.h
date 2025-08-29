0d0c669af4aedd1b3c69fdaa429cce4da0522d39
James Togher <jamestogher567@gmail.com>
2025-08-29 19:45:56 -0400

    Chunk class

---
+++ chunk.h
@@ -0,0 +1,28 @@
+#ifndef CHUNK_H
+#define CHUNK_H
+
+#include <cstdint>
+
+using namespace std;
+
+/*
+                            --------------------- CHUNK ---------------------
+                
+Chunks store instructions, we will read from chunks to get their instructions
+
+It will contain a count of the number of instructions.
+It will contain a capacity, or the current maximum amount of instructions it can currently hold
+It will contain an array (pointer) to uint8_t type, which will represent our bytecode instructions ex. 00000001
+
+Chunks will be dynamically doubled in size if the capacity < count + 1, functioning pretty much like a standard vector.
+
+*/
+
+class Chunk {
+
+    public:
+        Chunk(int count, int capacity, uint8_t* code);
+
+};
+
+#endif
