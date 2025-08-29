59ced4efa7028ea5c3094bfa38e028d264240679
James Togher <jamestogher567@gmail.com>
2025-08-29 19:49:14 -0400

    update notes

--- chunk.h
+++ chunk.h
@@ -12,10 +12,10 @@ Chunks store instructions, we will read from chunks to get their instructions
 
 It will contain a count of the number of instructions.
 It will contain a capacity, or the current maximum amount of instructions it can currently hold
-It will contain an array (pointer) to uint8_t type, which will represent our bytecode instructions ex. 00000001
+It will contain an array (pointer) to uint8_t type, which will store contiguous blocks of our bytecode instructions ex. 00000001
 
 Chunks will be dynamically doubled in size if the capacity < count + 1, functioning pretty much like a standard vector.
-
+In CPP I will probably opt for move operations over making copies, since moves are typically more efficient.
 */
 
 class Chunk {
